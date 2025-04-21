2ceeb9e16dab0b4e91dba64978c9022c1d22a622
trilvid <trilztech34@gmail.com>
2025-04-21 20:22:03 +0100

    updated route

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -36,15 +36,15 @@ const Contact = () => {
                 Any question? Reach out to us and we’ll get back to you shortly.
             </p>
             <div className="contact-card" data-aos="fade-up">
-                <a href='https://t.me/wealthflowinvest' className="ball contact-ball">
+                {/* <a href='https://t.me/wealthflowinvest' className="ball contact-ball">
                     <FaTelegramPlane />
-                </a>
+                </a> */}
                 <a href='mailto:wealthflowinvest@gmail.com' className="ball contact-ball">
                     <FiMail />
                 </a>
-                <a href="https://api.whatsapp.com/send?phone=16822969023"className="ball contact-ball">
+                {/* <a href="https://api.whatsapp.com/send?phone=16822969023"className="ball contact-ball">
                     <BsWhatsapp />
-                </a>
+                </a> */}
             </div>
         </div>
         <div className="contact-form-container">
